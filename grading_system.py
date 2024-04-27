11e1311e86a2b4f5634eac1e69d7edb386ce66a4
Subrat3066 <168131618+Subrat3066@users.noreply.github.com>
2024-04-28 01:11:39 +0530

    grading_system.py

---
+++ grading_system.py
@@ -0,0 +1,23 @@
+marks=int(input("Enter marks : "))
+if marks>90:
+    print("your grade is A+")
+elif marks>=85 and marks<=89:
+    print("Grade is A")
+elif marks>=80 and marks<=84:
+    print("Grade is A-")
+elif marks>=75 and marks<=79:
+    print("Grade is B+")
+elif marks>=70 and marks<=74:
+    print("Grade is B")
+elif marks>=65 and marks<=69:
+    print("Grade is C+")
+elif marks>=60 and marks<=64:
+    print("Grade is C")
+elif marks>=55 and marks<=69:
+    print("Grade is D+")
+elif marks>=50 and marks<=54:
+    print("Grade is D")
+elif marks>=40 and marks<=49:
+    print("Grade is E")
+elif marks>=0 and marks<=30:
+    print("Grade is F")
